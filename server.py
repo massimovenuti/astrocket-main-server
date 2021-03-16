0a8ebdf3f4c5156fd6627c269671a8ea9c5bb1ef
lsaunier <lsaunier@etu.unistra.fr>
2021-03-16 14:31:17 +0100

    Début du main server

---
+++ server.py
@@ -0,0 +1,27 @@
+#coding:utf-8
+
+import socket
+import requests
+
+host, port = ('',5566) 
+
+socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+socket.bind((host,port))
+print("Server is up and running ...\n")
+
+while True:
+    socket.listen(10) # 10 connexions échouée avant de refuser
+    conn, address = socket.accept()
+    print("Listening ...\n")
+
+    user_token = conn.recv(80)
+    user_token.decode("utf8")
+
+
+    # x = requests.post('https://auth.aw.alexandre-vogel.fr/',data = {'user_token' : user_token})
+
+
+
+conn.close()
+socket.close()
+    
