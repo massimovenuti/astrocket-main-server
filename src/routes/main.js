40e1561c58989d8fe4c690387022460f451aa77e
PHAN JEAN-LUC <jean-luc.phan@etu.unistra.fr>
2021-03-23 17:13:10 +0100

    Ajout du middleware aliveMainServer

--- src/routes/main.js
+++ src/routes/main.js
@@ -7,5 +7,6 @@ router.post('/GameServer', mainCtrl.addGameServer);
 router.get('/list', mainCtrl.listGameServer);
 router.delete('/GameServer', mainCtrl.deleteGameServer);
 router.put('/GameServer', mainCtrl.updateGameServer);
+router.post('/alive', mainCtrl.aliveMainServer);
 
 module.exports = router;
