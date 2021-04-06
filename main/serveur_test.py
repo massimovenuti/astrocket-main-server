4b6f5f2a57aa16781d8baaf347d901f68eb56896
lsaunier <lsaunier@etu.unistra.fr>
2021-04-06 15:09:31 +0200

    Légère modification + ajout requete lors du start des serveurs

--- main/serveur_test.py
+++ main/serveur_test.py
@@ -4,23 +4,32 @@ import subprocess
 import shlex
 import os
 import sys
+import requests
+import socket
 
 class GameServer:
-    def __init__(self, token, port):
-        self.token=token
+    def __init__(self, token, port, name):
+        self.token = token
         self.port = port
+        self.name = name
         self.proc = None
 
     def start(self):
         args = shlex.split("./exec {} {}".format(self.port, self.token))
         self.proc = subprocess.Popen(args, stdout=sys.stdout, stderr=sys.stderr)
 
+        hostname = socket.gethostname()
+        local_ip = socket.gethostbyname(hostname)
+
+        parameters = {'name':self.name, 'address':local_ip, 'port':self.port}
+        r = requests.post('https://main.aw.alexandre-vogel.fr/main/GameServer')
+
 def find_server_pid(pid, server_list):
     for server in server_list:
         if server.proc.pid == pid:
             return server
 
-def init_serveur():
+def init_server():
     server_list = []
     token_list = []
 
@@ -28,7 +37,7 @@ def init_serveur():
         for _, line in enumerate(fp):
             info = line.split(" ")
             token_list.append(info[0])
-            server_list.append(GameServer(info[0], info[1]))
+            server_list.append(GameServer(info[0], info[1], info[2]))
 
     return server_list, token_list
 
@@ -40,4 +49,15 @@ def manage_server(server_list):
         pid, status = os.wait()
         proc = find_server_pid(pid, server_list)
         proc.start
+
+def print_server(server_list):
+    for server in server_list:
+        print("Serveur " + server.name + " listening on port " + server.port,)
+
+
+print("Server running ...")
+
+server_list, token_list = init_server()
+print_server(server_list)
+# manage_server(server_list)
 
