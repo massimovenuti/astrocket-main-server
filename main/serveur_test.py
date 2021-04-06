1061e48628d9e7e0d013d4b021061c4b0e4889c7
lsaunier <lsaunier@etu.unistra.fr>
2021-04-06 15:12:33 +0200

    Début gestion erreur

--- main/serveur_test.py
+++ main/serveur_test.py
@@ -22,7 +22,10 @@ class GameServer:
         local_ip = socket.gethostbyname(hostname)
 
         parameters = {'name':self.name, 'address':local_ip, 'port':self.port}
-        r = requests.post('https://main.aw.alexandre-vogel.fr/main/GameServer')
+        r = requests.post('https://main.aw.alexandre-vogel.fr/main/GameServer', data=parameters)
+
+        if (r.status_code != 200):
+            print("Server " + self.name + " hasn't started") 
 
 def find_server_pid(pid, server_list):
     for server in server_list:
@@ -58,6 +61,6 @@ def print_server(server_list):
 print("Server running ...")
 
 server_list, token_list = init_server()
-print_server(server_list)
+# print_server(server_list)
 # manage_server(server_list)
 
