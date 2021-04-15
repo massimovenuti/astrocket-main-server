959c429e244e5a9e16a275b1ac76b017be6e818b
PHAN JEAN-LUC <jean-luc.phan@etu.unistra.fr>
2021-04-16 01:30:12 +0200

    Correction requete delete

--- main/life_check.py
+++ main/life_check.py
@@ -4,14 +4,14 @@ token = sys.argv[1]
 size = sys.argv[2]
 token_list = sys.argv[3:3+int(size)]
 server_list = sys.argv[3+int(size):]
-me = psutil.Process(os.getpid())
+parent = os.getppid()
 
-while me.parent is not None:
+while psutil.pid_exists(parent):
     print("Operating main server")
 
     parameters = []
-    for token in token_list:
-        parameters.append({"serverToken": token})
+    for servertoken in token_list:
+        parameters.append({"serverToken": servertoken})
     #r = requests.post('http://main.aw.alexandre-vogel.fr:3500/main/alive', json=parameters)
     r = requests.post('http://localhost:3500/main/alive', json=parameters)
 
@@ -24,13 +24,15 @@ while me.parent is not None:
         print("Server doesn't exist")
     else:
         print("BigMain doesn't respond")
-        
+
 print("Main server disappear")
 
+header = {"user_token":token}
 for i in range (len(server_list)):
-    parameters = {'name' : server_list[i]}
+    parameters = {'name':server_list[i]}
+    
     #r = requests.delete('http://main.alexandre-vogel.fr:3500/main/GameServer', json=parameters, headers=sys.argv[1])
-    r = requests.delete('http://localhost:3500/main/GameServer', json=parameters, headers=token)
+    r = requests.delete('http://localhost:3500/main/GameServer', json=parameters, headers=header)
 
     if (r.status_code == 200):
         print("Successfull deleting")
